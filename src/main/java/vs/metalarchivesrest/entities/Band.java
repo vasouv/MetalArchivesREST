578d63fbc53f04b9c6c93888380de6c0431fef6b
Souvatzis Vasilios <vasouv@gmail.com>
2017-08-11 13:17:35 +0300

    band info fetched correctly

---
+++ src/main/java/vs/metalarchivesrest/entities/Band.java
@@ -0,0 +1,66 @@
+package vs.metalarchivesrest.entities;
+
+import java.util.List;
+
+/**
+ *
+ * @author vasouv
+ */
+public class Band {
+    
+    private String id;
+    private String genre;
+    private String name;
+    private String photo;
+    private String bio;
+    private List<Album> albums;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public List<Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(List<Album> albums) {
+        this.albums = albums;
+    }
+    
+}
